12a578a70296d45e97e5d26af080f787f4263cf4
Pavel Berkal <pberkal@bk.ru>
2023-03-07 22:19:52 +0300

    add componentsNearestEntry and TestResults

--- src/pages/MainPage/TestResults/TestResults.tsx
+++ src/pages/MainPage/TestResults/TestResults.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@material-ui/core';
+import {ReactComponent as Arrow} from '../assets/Arrow.svg'
+import {ReactComponent as Rabbits} from '../assets/Rabbits.svg'
 import classes from './TestResults.module.scss'
 
 const TestResults = () => {
     return (
         <div className={classes.TestResults}>
-            TestResults
+            <div className={classes.resultsTitle}>Результаты анализов</div>
+            <div className={classes.resultsInfo}>
+                Готов анализ на пироплазмов
+                <span>12.02.23, Кеша</span>
+            </div>
+            <div className={classes.resultsMore}>
+                <Rabbits/>
+                <Button className={classes.button}>
+                    <div className={classes.text}>Подробнее</div>
+                    <Arrow/>
+                </Button>
+            </div>
         </div>
     );
 };
