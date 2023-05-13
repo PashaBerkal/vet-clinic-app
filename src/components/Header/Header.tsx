3ffc3f4aed8f120654587db8e30c709fb2859a11
Pavel Berkal <pberkal@bk.ru>
2023-05-13 18:08:39 +0300

    add auth layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,34 @@ import HeaderInfo from './HeaderInfo';
 import NavBar from './NavBar';
 import Container from '../../hoc/Container';
 import classes from './Header.module.scss';
+import { useAppSelector } from '../../hooks/redux';
 
-const Header = () => (
-  <div className={classes.Header}>
-    <Container>
-      <HeaderInfo />
-    </Container>
-    <div className={classes.line} />
-    <Container>
-      <NavBar />
-    </Container>
-  </div>
-);
+const Header = () => {
+  const { isAuth } = useAppSelector((state) => state.auth);
+  return (
+
+    <div>
+      {isAuth
+        ? (
+          <div className={classes.Header}>
+            <Container>
+              <HeaderInfo />
+            </Container>
+            <div className={classes.line} />
+            <Container>
+              <NavBar />
+            </Container>
+          </div>
+        )
+        : (
+          <div className={classes.HeaderAuth}>
+            <Container>
+              <HeaderInfo />
+            </Container>
+          </div>
+        )}
+    </div>
+  );
+};
 
 export default Header;
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -9,11 +9,15 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
 import { useNavigate } from 'react-router-dom';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Container from '../../hoc/Container/Container';
 import { AuthRequestParams } from '../../models/IAuth';
 import { useAppDispatch } from '../../hooks/redux';
 import { useLoginMutation } from '../../redux/auth/authApiSlice';
 import { setCredentials } from '../../redux/auth/authSlice';
+import catImg from './assets/cat.png';
+import classes from './AuthPage.module.scss';
 
 const AuthPage = () => {
   const dispatch = useAppDispatch();
@@ -51,41 +55,64 @@ const AuthPage = () => {
 
   return (
     <Container>
-      <div>
-        <TextField
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-          onChange={handleChange}
-          value={values.email}
-          name="email"
-        />
-        <FormControl variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={isShowPassword ? 'text' : 'password'}
-            name="password"
-            value={values.password}
+      <div className={classes.Auth}>
+        <div className={classes.welocme}>
+          <div className={classes.title}>Добро пожаловать в клинику «Кот и доГ»!</div>
+          <div className={classes.subtitle}>
+            Войдите в личный кабинет, чтобы следить за здоровьем
+            <br />
+            своего питомца или записать его на прием в клинику.
+          </div>
+          <div className={classes.img}>
+            <img src={catImg} alt="кот" />
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.title}>
+            Войти в личный кабинет
+          </div>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
             onChange={handleChange}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {isShowPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+            value={values.email}
+            name="email"
+            className={classes.email}
           />
-        </FormControl>
-        <Button variant="contained" size="large" onClick={authHandler}>
-          Войти
-        </Button>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={isShowPassword ? 'text' : 'password'}
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              className={classes.password}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    className="togglePass"
+                  >
+                    {isShowPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+            }
+              label="Password"
+            />
+          </FormControl>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Запомнить меня" />
+          <Button variant="contained" size="large" onClick={authHandler} className={classes.button}>
+            Войти
+          </Button>
+          <div className={classes.forgot}>
+            Не помню пароль
+          </div>
+        </div>
       </div>
     </Container>
   );
